e1ef8e196cbc59b7d18f9f593128885de98d5d76
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-11-08 11:28:45 +0200

    implement payment intent in video 52

---
+++ lib/core/utils/api_keys.dart
@@ -0,0 +1,4 @@
+class ApiKeys {
+  static String secretKey =
+      'sk_test_51SJYJYFfRg9a1yXfMGtzQp3WfR20Y57Ooj2tyzoTNifrtINZNm12l2ze1B1ttDt13AtGA1gaJ9gok6D5Bhvnwtdk00yWLuJ6Yc';
+}
--- lib/core/utils/stripe_services.dart
+++ lib/core/utils/stripe_services.dart
@@ -1,9 +1,23 @@
+import 'package:paypal_integreation/core/utils/api_keys.dart';
+import 'package:paypal_integreation/core/utils/api_service.dart';
 import 'package:paypal_integreation/features/checkout/data/model/payment_intent_input_model.dart';
 
 import '../../features/checkout/data/model/payment_intent_model/payment_intent_model.dart';
 
 class StripeServices {
-  // PaymentIntentModel createPaymentIntent(
-  //   PaymentIntentInputModel paymentIntentInputModel,
-  // ) {}
+  final ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentIntent(
+    PaymentIntentInputModel paymentIntentInputModel,
+  ) async {
+    var response = await apiService.post(
+      body: paymentIntentInputModel.toJson(),
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: ApiKeys.secretKey,
+      //'sk_test_51SJYJYFfRg9a1yXfMGtzQp3WfR20Y57Ooj2tyzoTNifrtINZNm12l2ze1B1ttDt13AtGA1gaJ9gok6D5Bhvnwtdk00yWLuJ6Yc',
+    );
+
+    var paymantIntentModel = PaymentIntentModel.fromJson(response.data);
+
+    return paymantIntentModel;
+  }
 }
