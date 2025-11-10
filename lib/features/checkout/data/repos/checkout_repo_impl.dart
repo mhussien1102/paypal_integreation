ae3a9d683eaeb35f9289663a09cdc0885569e569
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-11-10 21:52:12 +0200

    checkout repo impl in video 57

---
+++ lib/features/checkout/data/repos/checkout_repo_impl.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:paypal_integreation/core/errors/failure.dart';
+import 'package:paypal_integreation/core/utils/stripe_services.dart';
+import 'package:paypal_integreation/features/checkout/data/model/payment_intent_input_model.dart';
+import 'package:paypal_integreation/features/checkout/data/repos/checkout_repo.dart';
+
+class CheckOutRepoImpl extends CheckOutRepo {
+  final StripeServices stripeService = StripeServices();
+  @override
+  Future<Either<Failure, void>> makePayment({
+    required PaymentIntentInputModel paymentIntentInputModel,
+  }) async {
+    try {
+      await stripeService.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel,
+      );
+
+      return right(null);
+    } catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+}
